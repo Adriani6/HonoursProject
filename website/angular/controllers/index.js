df73881eb882f6a082ad6afd81ed390186043e2c
Adrian Iwaszkiewicz <adrian@adriani6.co.uk>
2017-02-07 20:02:32 +0000

    Finished Basic Template Node Server Quick Implementation
    Signed-off-by: Adrian Iwaszkiewicz <adrian@adriani6.co.uk>

---
+++ website/angular/controllers/index.js
@@ -0,0 +1,42 @@
+var index = angular.module("index", ['ui.bootstrap']);
+
+index.controller('LoginCtrl', function ($uibModal, $document) {
+  var $ctrl = this;
+  $ctrl.items = ['item1', 'item2', 'item3'];
+
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.open = function (size, parentSelector) {
+    var parentElem = parentSelector ? 
+      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+  };
+  
+});
+
+index.controller("ModalInstanceCtrl", function($uibModalInstance)
+{
+
+    var $ctrl = this;
+
+    $ctrl.login = function()
+    {
+        alert("Logging in...");
+    }
+
+})
