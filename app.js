5598e8f3950c74672b3e666e68cf8dc7b1fb82b0
Adrian Iwaszkiewicz <adrian@adriani6.co.uk>
2017-02-08 21:39:49 +0000

    Beginning of API Implementation

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 var express = require('express');
+var router = express.Router();
 var app = express();
+var appApi = require("./api/api.js");
+
+var api = new appApi();
+
+app.use('/', express.static(__dirname + '/website'));
+app.use("/api", router);
 
 var server = app.listen(80);
+
+app.get("/", function(req, res)
+{
+    res.sendFile("index.html")
+})
+
+router.get("/", function(req, res)
+{
+    res.send("API Home Page")
+})
+
+//User Requests
+router.get("/user/new", api.user.new)
